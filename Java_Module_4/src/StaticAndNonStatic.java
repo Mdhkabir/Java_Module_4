476bf4c9d6f4d797aa9edc65541037775105f4a9
Admin <Admin@MD-Kabir>
2017-01-28 13:58:30 -0500

    Initial Checkin

---
+++ Java_Module_4/src/StaticAndNonStatic.java
@@ -0,0 +1,24 @@
+
+public class StaticAndNonStatic {
+	
+	public static int p = 200; // This is Global static variable
+	
+	public int q = 100; // This is Global non static variable
+
+
+	//static method
+	public static void first_function(){
+		int abc = 20; // This abc is a local variable
+	}
+	// non static method
+	public void second_function(){
+		int xyz = 100; //This abc is a local variable
+	}
+	
+	
+	
+	
+	
+	
+	
+}
